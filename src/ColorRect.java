63b3d7093e24a88d8b3d24e290b9b59ec9a6d229
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-09-15 20:46:43 +0300

    Initial commit

---
+++ src/ColorRect.java
@@ -0,0 +1,8 @@
+public class ColorRect extends Rect{
+    public ColorRect(double a, double b) {
+        super(a, b);
+    }
+    public ColorRect(double size){
+        super(size);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Triangle triangle1 = new Triangle(10,20,20);
+        System.out.println(triangle1.area());
+    }
+}
